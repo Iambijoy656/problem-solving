a50f49c7570bf43c49c8a3d85fd64323c5909558
Bijoy Chandro Das <freelancing.bijoy@gmail.com>
2022-12-19 23:33:07 +0600

    find max num

--- problem-solving/findMaxAndMinNum.js
+++ problem-solving/findMaxAndMinNum.js
@@ -5,14 +5,25 @@ you have been provide an array of number
 you have to find Minimum number
  */
 
-function min(arr) {
-    let minNum = arr[0];
-    for (let num of arr) {
-        if (num < minNum) {
-            minNum = num;
-        }
+// function min(arr) {
+//     let minNum = arr[0];
+//     for (let num of arr) {
+//         if (num < minNum) {
+//             minNum = num;
+//         }
 
-    }
+//     }
+//     return minNum;
+// }
+
+
+// const arr = [10, 20, 3, 40, 50, 6]
+// console.log(min(arr));
+
+
+
+function min(arr) {
+    const minNum = Math.min(...arr)
     return minNum;
 }
 
@@ -20,3 +31,39 @@ function min(arr) {
 const arr = [10, 20, 3, 40, 50, 6]
 console.log(min(arr));
 
+
+
+
+
+/**
+Find max and min sum 
+
+you have been provide an array of number
+you have to find Minimum number
+ */
+
+// function min(arr2) {
+//     let maxNum = arr[0];
+//     for (let num of arr2) {
+//         if (num > maxNum) {
+//             minNum = num;
+//         }
+
+//     }
+//     return minNum;
+// }
+
+
+// const arr2 = [10, 20, 3, 40, 50, 6]
+// console.log(min(arr2));
+
+
+
+function min(arr2) {
+    const minNum = Math.max(...arr2)
+    return minNum;
+}
+
+
+const arr2 = [10, 20, 3, 40, 50, 6]
+console.log(min(arr2));
