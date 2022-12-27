44d0b9c35cac0cc0de3257afa4a4ab82299056c0
Bijoy Chandro Das <freelancing.bijoy@gmail.com>
2022-12-28 02:07:49 +0600

    sum  zero solve

--- usingMultiplePointerTechnique/sumZero.js
+++ usingMultiplePointerTechnique/sumZero.js
@@ -16,8 +16,45 @@
 //sumZero([1,2,3]) output: undefined
 
 function sumZero(arr) {
+    let left = 0;
+    let right = arr.length-1;
+
+    while (left < right) {
+        let sum = arr[left] + arr[right];
+
+        if (sum  === 0) {
+            return [arr[left],arr[right] ]
+        }
+        if (sum  > 0) {
+           right --
+        }
+        if (sum  < 0) {
+            left++;
+        }
+
+    }
+
+    return undefined;
 
 
 }
 
 console.log(sumZero([-3, -2, 1, 0, 1, 2, 3]));
+console.log(sumZero([-2,0,2,3]));
+console.log(sumZero([1,2,3]));
+
+// [-3, -2, 1, 0, 1, 2, 3]
+    // l                r
+    // -3+3 =0 [-3,3]
+
+// [-2,0,2,3]
+ // l    r
+    // -2+3 = 1
+    // -2+2 =0 [-2,2]
+
+// [1,2,3]
+ // l   
+// r 
+// 1+3 = 4
+ // 1+2 = 3
+ // 1+1 = 0
