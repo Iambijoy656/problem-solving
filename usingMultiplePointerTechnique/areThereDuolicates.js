469e79296f3e481304729c0423b738818ac228b0
Bijoy Chandro Das <freelancing.bijoy@gmail.com>
2022-12-29 00:11:37 +0600

    are There any duplicates

---
+++ usingMultiplePointerTechnique/areThereDuolicates.js
@@ -0,0 +1,21 @@
+/**
+ * Write a function called areThereDuplicates which accepts a sorted array of integers
+ * The function should  return true if there is any duplicates and false if there is no duplicates
+ */
+
+function areThereAnyDuplicates(arr) {
+    let i = 0;
+    let j = 1;
+    while (i < arr.length) {
+        if (arr[i] === arr[j]) {
+            return true;
+        }
+        i++;
+        j++;
+    }
+    return false;
+
+}
+
+console.log(areThereAnyDuplicates([1, 1, 1, 1, 1, 2]))
+console.log(areThereAnyDuplicates([-2, -1, 0, 1, 2]))
