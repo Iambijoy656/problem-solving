00f0eaba89a059d0be0115a6b152f9bd34ce7736
Bijoy Chandro Das <freelancing.bijoy@gmail.com>
2023-01-09 15:55:21 +0600

    collect string

---
+++ problem-solving/collectString.js
@@ -0,0 +1,47 @@
+//  Write a function called collectStrings which accepts an object and returns an array of all the values in the object that have a typeof string
+
+
+//helper method recursion
+
+function collectString(obj) {
+    let stringsArr = []
+
+    function gatherString(o) {
+        for (const key in o) {
+            if (typeof o[key] === 'object') {
+                return gatherString(o[key])
+            } else {
+                stringsArr.push(o[key])
+            }
+
+        }
+
+
+    }
+    gatherString(obj)
+    console.log(stringsArr)
+
+
+}
+
+
+
+
+const obj = {
+    stuff: "foo",
+    data: {
+        val: {
+            thing: {
+                info: "bar",
+                moreInfo: {
+                    evenMoreInfo: {
+                        weMadeIt: "baz"
+                    }
+                }
+            }
+        }
+    }
+}
+
+collectString(obj) // ["foo", "bar", "baz"])
+
